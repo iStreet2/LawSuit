0bb3949813b40d434b9a0f05583e5bd6330d4f4b
Gabriel Vicentin Negro <gabrielvicentinnegro@hotmail.com>
2024-08-09 14:36:33 -0300

    Criar o projeto no Xcode

---
+++ LawSuit/LawSuit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LawSuit
+//
+//  Created by Gabriel Vicentin Negro on 09/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LawSuit/LawSuit/LawSuitApp.swift
@@ -0,0 +1,17 @@
+//
+//  LawSuitApp.swift
+//  LawSuit
+//
+//  Created by Gabriel Vicentin Negro on 09/08/24.
+//
+
+import SwiftUI
+
+@main
+struct LawSuitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
