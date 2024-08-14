636080173986ea71dc3d3b31e73926db20f4fcb0
Gabriel Vicentin Negro <gabrielvicentinnegro@hotmail.com>
2024-08-14 17:46:44 -0300

    criar view do icone da pasta

---
+++ LawSuit/LawSuit/Views/Components/FolderIconView.swift
@@ -0,0 +1,20 @@
+//
+//  FolderIconView.swift
+//  LawSuit
+//
+//  Created by Gabriel Vicentin Negro on 14/08/24.
+//
+
+import SwiftUI
+
+struct FolderIconView: View {
+    var body: some View {
+        VStack {
+            Text("Hello World")
+        }
+    }
+}
+
+#Preview {
+    FolderIconView()
+}
