cc8b2cc22d72f02e094dd8cb401f12777390074d
Gabriel Vicentin Negro <gabrielvicentinnegro@hotmail.com>
2024-08-19 15:10:08 -0300

    atualizar multiplos modelos de entidade no coredata, desenvolver mais a DocumentView, com mais features de criar pasta, apagar pasta, renomear pasta

--- LawSuit/LawSuit/Model/CoreDataModel/FilePDF.swift
+++ LawSuit/LawSuit/Model/CoreDataModel/FilePDF.swift
@@ -2,7 +2,7 @@
 //  FilePDF+CoreDataClass.swift
 //  LawSuit
 //
-//  Created by Gabriel Vicentin Negro on 15/08/24.
+//  Created by Gabriel Vicentin Negro on 19/08/24.
 //
 //
 
@@ -11,14 +11,14 @@ import CoreData
 
 @objc(FilePDF)
 public class FilePDF: NSManagedObject, Identifiable {
-
+    
     @nonobjc public class func fetchRequest() -> NSFetchRequest<FilePDF> {
         return NSFetchRequest<FilePDF>(entityName: "FilePDF")
     }
 
-    @NSManaged public var content: Data
-    @NSManaged public var id: String
-    @NSManaged public var name: String
+    @NSManaged public var content: Data?
+    @NSManaged public var id: String?
+    @NSManaged public var name: String?
     @NSManaged public var parentFolder: Folder?
     @NSManaged public var update: Update?
 }
--- LawSuit/LawSuit/Views/Components/DocumentGridView.swift
+++ LawSuit/LawSuit/Views/Components/DocumentGridView.swift
@@ -10,49 +10,45 @@ import SwiftUI
 struct DocumentGridView: View {
     
     //MARK: Variáveis de estado
-    @ObservedObject var rootFolder: Folder
+    var parentFolder: Folder
     
+    //MARK: ViewModels
     @EnvironmentObject var folderViewModel: FolderViewModel
+    @EnvironmentObject var dragAndDropViewModel: DragAndDropViewModel
     
     //MARK: CoreData
     @EnvironmentObject var coreDataViewModel: CoreDataViewModel
     @FetchRequest var folders: FetchedResults<Folder>
     
-    init(rootFolder: Folder) {
-        self.rootFolder = rootFolder
+    init(parentFolder: Folder) {
+        self.parentFolder = parentFolder
         _folders = FetchRequest<Folder>(
-            sortDescriptors: [],
-            predicate: NSPredicate(format: "parentFolder == %@", rootFolder)
+            sortDescriptors: []
+            ,predicate: NSPredicate(format: "parentFolder == %@", parentFolder)
         )
     }
     
     //MARK: Calculo da grid
     let spacing: CGFloat = 10
-    let itemWidth: CGFloat = 70
+    let itemWidth: CGFloat = 90
     
     var body: some View {
         GeometryReader { geometry in
             let columns = Int(geometry.size.width / (itemWidth + spacing))
             let gridItems = Array(repeating: GridItem(.flexible(), spacing: spacing), count: max(columns, 1))
-            NavigationStack {
-                ScrollView {
-                    VStack {
-                        LazyVGrid(columns: gridItems, spacing: spacing) {
-                            ForEach(folders, id: \.self) { folder in
-                                FolderIconView(folder: folder)
-                                    .onTapGesture(count: 2) {
-                                        folderViewModel.openFolder(folder: folder)
-                                    }
-                            }
+            ScrollView {
+                VStack {
+                    LazyVGrid(columns: gridItems, spacing: spacing) {
+                        ForEach(folders, id: \.self) { folder in
+                            FolderIconView(folder: folder, parentFolder: parentFolder)
+                                .onTapGesture(count: 2) {
+                                    folderViewModel.openFolder(folder: folder)
+                                }
+                                .padding(.leading)
                         }
                     }
                 }
             }
-        }    }
+        }
+    }
 }
-
-//#Preview {
-//    DocumentGridView(rootFolder: FolderExamples.folderExample())
-//        .environmentObject(CoreDataViewModel())
-//        .environmentObject(FolderViewModel())
-//}
