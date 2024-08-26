14a473570cc42eac2411d40f0a81d59b3c76f0c1
Sonzzini <paulo.sonzzini@gmail.com>
2024-08-26 16:46:17 -0300

    Salvar na nuvem a partir de modelos CoreData e realizar um fetch dos objetos em nuvem e transformá-los em objetos CoreData

---
+++ LawSuit/LawSuit/ViewModel/Managers/RecordObjectManager.swift
@@ -0,0 +1,101 @@
+//
+//  RecordObjectManager.swift
+//  LawSuit
+//
+//  Created by Paulo Sonzzini Ribeiro de Souza on 26/08/24.
+//
+
+import Foundation
+import CloudKit
+import CoreData
+import PDFKit
+
+class RecordObjectManager {
+	let context: NSManagedObjectContext
+	let container = CKContainer(identifier: "iCloud.com.TFS.LawSuit.CKContainer")
+	let publicDatabase: CKDatabase
+	
+	init(context: NSManagedObjectContext) {
+		self.context = context
+		publicDatabase = container.publicCloudDatabase
+	}
+	
+	
+	func makeObjectsFromRecords(records: [CKRecord]) async -> [Any]? {
+		
+		var objects: [Any] = []
+		
+		for record in records {
+			switch record.recordType {
+			case "FilePDF":
+				let fileObject = FilePDF(context: context)
+				
+				if let name = record["name"] as? String {
+					fileObject.name = name
+				}
+				if let id = record["id"] as? String {
+					fileObject.id = id
+				}
+				if let asset = record["content"] as? CKAsset {
+					guard let bin = asset.fileURL else { return nil }
+					do {
+						let pdfData = try Data(contentsOf: bin)
+						
+						fileObject.content = pdfData
+					} catch {
+						print("Could not get pdf Data as BIN: \(error)")
+					}
+				}
+				if let recordName = record["recordName"] as? String {
+					fileObject.recordName = recordName
+				}
+				
+				objects.append(fileObject)
+			
+			case "Folder":
+				let folderObject = Folder(context: context)
+				
+				if let name = record["name"] as? String {
+					folderObject.name = name
+				}
+				if let id = record["id"] as? String {
+					folderObject.id = id
+				}
+				if let recordName = record["recordName"] as? String {
+					folderObject.recordName = recordName
+				}
+				if let folderReferences = record["folders"] as? [CKRecord.Reference] {
+					for reference in folderReferences {
+						do {
+							let relatedRecord = try await publicDatabase.record(for: reference.recordID)
+							if let relatedObject = await makeObjectsFromRecords(records: [relatedRecord]) as? [Folder] {
+								folderObject.addToFolders(relatedObject.first!)
+							}
+						} catch {
+							print("Error in relating folderRecord to reference: \(error)")
+						}
+					}
+				}
+				if let fileReferences = record["files"] as? [CKRecord.Reference] {
+					for reference in fileReferences {
+						do {
+							let relatedRecord = try await publicDatabase.record(for: reference.recordID)
+							if let relatedObject = await makeObjectsFromRecords(records: [relatedRecord]) as? [FilePDF] {
+								folderObject.addToFiles(relatedObject.first!)
+							}
+						} catch {
+							print("Error in relation objectRecord to reference: \(error)")
+						}
+					}
+				}
+				
+				objects.append(folderObject)
+				
+			default:
+				return nil
+			}
+		}
+		
+		return objects
+	}
+}
