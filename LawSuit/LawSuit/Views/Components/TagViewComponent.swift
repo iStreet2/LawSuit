e64fe793d7849306ab1bafb08b020e2a7532cc30
Sonzzini <paulo.sonzzini@gmail.com>
2024-08-28 16:31:12 -0300

    implementar a quinta versão do tagViewPickerComponent

--- LawSuit/LawSuit/Views/Components/TagViewComponent.swift
+++ LawSuit/LawSuit/Views/Components/TagViewComponent.swift
@@ -303,6 +303,72 @@ struct TagViewPickerComponentV4: View {
 	}
 }
 
+struct TagViewPickerComponentV5: View {
+
+	@Environment(\.defaultMinListRowHeight) var listRowHeight
+	
+	@Binding var currentTag: TagType
+	@State var stateCurrentTag: TagType = .trabalhista
+	@State var isShowingPickerView: Bool = false
+
+	@State private var hoveredTag: TagType?
+	
+	let allTags: [TagType] = TagType.allCases
+	
+	
+	var body: some View {
+		VStack {
+			TagViewComponent(tagType: stateCurrentTag, isPicker: true)
+				.onTapGesture {
+					withAnimation(.easeInOut(duration: 0.5)) {
+						isShowingPickerView.toggle()
+					}
+				}
+			if isShowingPickerView {
+				List(allTags) { tag in
+					VStack {
+						Text(tag.tagText)
+							.foregroundStyle(hoveredTag == tag ? tag.tagColorForeground : .white)
+					}
+					.onHover { hovering in
+						withAnimation(.easeInOut(duration: 0.2)) {
+							if hovering {
+								hoveredTag = tag
+							} else {
+								hoveredTag = nil
+							}
+						}
+					}
+					.onTapGesture {
+						if #available(macOS 14.0, *) {
+							withAnimation(.easeInOut(duration: 0.5)) {
+								currentTag = tag
+								stateCurrentTag = tag
+							} completion: {
+								withAnimation(.easeInOut(duration: 0.5)) {
+									isShowingPickerView.toggle()
+								}
+							}
+						} else {
+							// Fallback on earlier versions
+							withAnimation(.easeInOut(duration: 0.5)) {
+								currentTag = tag
+								stateCurrentTag = tag
+								isShowingPickerView.toggle()
+							}
+						}
+					}
+				}
+				.frame(width: 150, height: listRowHeight * CGFloat(allTags.count) + 20)
+				.clipShape(RoundedRectangle(cornerRadius: 12))
+			}
+		}
+		.onAppear {
+			stateCurrentTag = currentTag
+		}
+	}
+}
+
 
 extension Color {
 	 init(hex: String) {
@@ -402,6 +468,7 @@ extension Color {
 		TagViewPickerComponentV2(currentTag: .constant(.tributario))
 		TagViewPickerComponentV3(currentTag: .constant(.penal))
 		TagViewPickerComponentV4(currentTag: .constant(.civel))
+		TagViewPickerComponentV5(currentTag: .constant(.tributario))
 		Text("Wow!").foregroundStyle(.black)
 		Text("Wow!").foregroundStyle(.black)
 		Text("Wow!").foregroundStyle(.black)
