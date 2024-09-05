b888a82521824130c2df1c82b4de4c010db7ccc0
Gabriel Vicentin Negro <gabrielvicentinnegro@hotmail.com>
2024-09-05 17:29:06 -0300

    modificar as funções dos managers de client e lawsuit para aceitar os novos atributos dos modelos

--- LawSuit/LawSuit/ViewModel/Managers/ClientManager.swift
+++ LawSuit/LawSuit/ViewModel/Managers/ClientManager.swift
@@ -17,38 +17,13 @@ class ClientManager {
         self.context = context
     }
     
-    func createClient(
-        name: String,
-        occupation: String,
-        rg: String,
-        cpf: String,
-        lawyer: Lawyer,
-        affiliation: String,
-        maritalStatus: String,
-        nationality: String,
-        birthDate: Date,
-        cep: String,
-        address: String,
-        addressNumber: String,
-        neighborhood: String,
-        complement: String,
-        state: String,
-        city: String,
-        email: String,
-        telephone: String,
-        cellphone: String
-    ) {
+    func createClient(name: String, occupation: String, rg: String, cpf: String, lawyer: Lawyer, affiliation: String, maritalStatus: String, nationality: String, birthDate: Date, cep: String, address: String, addressNumber: String, neighborhood: String, complement: String, state: String, city: String, email: String, telephone: String, cellphone: String) {
         let client = Client(context: context)
-        
-        // Criação da pasta raiz
         let folder = Folder(context: context)
         folder.name = "\(name)"
         folder.id = "root\(name)"
-        
         client.rootFolder = folder
         folder.parentClient = client
-        
-        // Atributos
         client.name = name
         client.id = UUID().uuidString
         client.occupation = occupation
@@ -69,7 +44,6 @@ class ClientManager {
         client.email = email
         client.telephone = telephone
         client.cellphone = cellphone
-        
         saveContext()
     }
     
@@ -79,28 +53,7 @@ class ClientManager {
 //        lawyer.removeFromClients(client)
     }
     
-    func editClient(
-        client: Client,
-        name: String,
-        occupation: String,
-        rg: String,
-        cpf: String,
-        affiliation: String,
-        maritalStatus: String,
-        nationality: String,
-        birthDate: Date,
-        cep: String,
-        address: String,
-        addressNumber: String,
-        neighborhood: String,
-        complement: String,
-        state: String,
-        city: String,
-        email: String,
-        telephone: String,
-        cellphone: String
-    ) {
-        // Atualiza os atributos do cliente
+    func editClient(client: Client, name: String, occupation: String, rg: String, cpf: String, affiliation: String, maritalStatus: String, nationality: String, birthDate: Date, cep: String, address: String, addressNumber: String, neighborhood: String, complement: String, state: String, city: String, email: String, telephone: String, cellphone: String) {
         client.name = name
         client.occupation = occupation
         client.rg = rg
@@ -120,12 +73,8 @@ class ClientManager {
         client.email = email
         client.telephone = telephone
         client.cellphone = cellphone
-        
-        // Salva o contexto para persistir as mudanças
         saveContext()
     }
-
-
     
     func addPhotoOnClient(client: Client, photo: Data) {
         client.photo = photo
