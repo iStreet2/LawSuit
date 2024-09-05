e0b0368f4a6088c82bb820bab4fdfe90aabd7622
Gabriel Vicentin Negro <gabrielvicentinnegro@hotmail.com>
2024-09-05 17:32:46 -0300

    criar um manager para "Entity"

---
+++ LawSuit/LawSuit/ViewModel/Managers/EntityManager.swift
@@ -0,0 +1,42 @@
+//
+//  EntityManager.swift
+//  LawSuit
+//
+//  Created by Gabriel Vicentin Negro on 05/09/24.
+//
+
+import Foundation
+import CoreData
+
+class EntityManager {
+    
+    var context: NSManagedObjectContext
+    
+    init(context: NSManagedObjectContext) {
+        self.context = context
+    }
+    
+    func createEntity(name: String) {
+        var entity = Entity(context: context)
+        entity.id = UUID().uuidString
+        entity.name = name
+        saveContext()
+    }
+    
+    func deleteEntity(entity: Entity) {
+        context.delete(entity)
+    }
+    
+    func editEntity(entity: Entity, name: String) {
+        entity.name = name
+        saveContext()
+    }
+    
+    func saveContext() {
+        do {
+            try context.save()
+        } catch {
+            print("Error saving entity: \(error)")
+        }
+    }
+}
