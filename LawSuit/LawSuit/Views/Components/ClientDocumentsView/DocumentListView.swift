f2410ed0ab31dbb0b9950de3b99954b484c3d931
Gabriel Vicentin Negro <gabrielvicentinnegro@hotmail.com>
2024-10-15 12:46:57 -0300

    retirar essas funções

--- LawSuit/LawSuit/Views/Components/ClientDocumentsView/DocumentListView.swift
+++ LawSuit/LawSuit/Views/Components/ClientDocumentsView/DocumentListView.swift
@@ -52,10 +52,8 @@ struct DocumentListView: View {
                 .font(.footnote)
                 .bold()
                 .foregroundStyle(Color(.gray))
-                
                 VStack(alignment: .leading) {
                     Divider()
-                    
                     if openFolder.folders!.count == 0 && openFolder.files!.count == 0{
                         HStack {
                             Spacer()
@@ -64,26 +62,19 @@ struct DocumentListView: View {
                                 .frame(height: geometry.size.height / 2)
                             Spacer()
                         }
-                        
                     }
-                    
                     ScrollView {
-                        
                         VStack(alignment: .leading) {
                             FolderView(parentFolder: openFolder, geometry: geometry)
                                 .onTapGesture(count: 2) {
                                     folderViewModel.openFolder(folder: openFolder)
                                 }
                             FilePDFGridView(parentFolder: openFolder, geometry: geometry)
-                            
                         }
                         .padding(.leading, 10)
-                        
                     }
-                    
                 }
                 .background(.black.opacity(0.01))
-                
             }
             .padding(.top, 11)
             .onChange(of: openFolder) { _ in
@@ -97,7 +88,6 @@ struct DocumentListView: View {
                     Text("Nova Pasta")
                     Image(systemName: "folder")
                         .resizable()
-                    
                 })
                 Button {
                     folderViewModel.importPDF(parentFolder: openFolder, dataViewModel: dataViewModel)
