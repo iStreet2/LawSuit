b1ccb8feb563774aafa013cff0f175e704f9b9f4
Gabriel Vicentin Negro <gabrielvicentinnegro@hotmail.com>
2024-10-17 17:41:10 -0300

    criar o arquivo do empty state

---
+++ LawSuit/LawSuit/Views/Components/EmptyStates/LawsuitsEmptyState.swift
@@ -0,0 +1,18 @@
+//
+//  LawsuitsEmptyState.swift
+//  LawSuit
+//
+//  Created by Gabriel Vicentin Negro on 17/10/24.
+//
+
+import SwiftUI
+
+struct LawsuitsEmptyState: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LawsuitsEmptyState()
+}
