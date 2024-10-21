3c78b15b95b9504a2865af73a4aa4e54cd77435a
Gabriel Vicentin Negro <gabrielvicentinnegro@hotmail.com>
2024-10-21 14:24:19 -0300

    começar a implemenar o EmptyState de processos

--- LawSuit/LawSuit/Views/Components/EmptyStates/ClientEmptyState.swift
+++ LawSuit/LawSuit/Views/Components/EmptyStates/ClientEmptyState.swift
@@ -16,7 +16,7 @@ struct ClientEmptyState: View {
         ZStack {
             Image("clientEmptyStateBackground")
                 .resizable()
-                .scaledToFit()
+                .scaledToFill()
             VStack {
                 if clients.count == 0 {
                     Text("Dê o primeiro passo para uma gestão \n jurídica mais eficiente!")
--- LawSuit/LawSuit/Views/Components/EmptyStates/LawsuitsEmptyState.swift
+++ LawSuit/LawSuit/Views/Components/EmptyStates/LawsuitsEmptyState.swift
@@ -8,8 +8,40 @@
 import SwiftUI
 
 struct LawsuitsEmptyState: View {
+    
+    //Fetch de clientes
+    //Fetch de processos
+    //Binding de toogle de adicionar cliente
+    //Binding de toogle de adicionar processo
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Image("clientEmptyStateBackground")
+                .resizable()
+                .scaledToFill()
+            VStack {
+                if true {
+                    Text("Dê o primeiro passo para uma gestão \n jurídica mais eficiente!")
+                        .font(.title)
+                        .multilineTextAlignment(.center)
+                        .foregroundStyle(.grayText)
+                        .bold()
+                    Button {
+                        //Adicionar processo
+                    } label: {
+                        Text("Adicionar Cliente")
+                    }
+                    .buttonStyle(.borderedProminent)
+                    .tint(.arqBlack)
+                } else {
+                    Text("Selecione um cliente")
+                        .font(.title)
+                        .multilineTextAlignment(.center)
+                        .bold()
+                        .foregroundStyle(.grayText)
+                }
+            }
+        }
     }
 }
 
