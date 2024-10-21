7bb8261e8a4f77f6fb3c0902457fb97bbc06fb5d
Emily Morimoto <111714314+emilymorimoto@users.noreply.github.com>
2024-10-21 16:30:39 -0300

    fix contactsManager

--- LawSuit/LawSuit/ViewModel/Managers/ContactsManager.swift
+++ LawSuit/LawSuit/ViewModel/Managers/ContactsManager.swift
@@ -9,19 +9,12 @@ import Foundation
 import Contacts
 import SwiftUI
 
-enum AlertMessageEnum: String {
-    case accessDenied = "Acesso aos contatos foi negado. Vá até as Preferências do Sistema > Segurança e Privacidade para conceder acesso ao app."
-    case accessRestricted = "O acesso aos contatos está restrito e não pode ser autorizado."
-    case unknownError = "Erro desconhecido ao acessar contatos."
-}
-
-class ContactsManager: ObservableObject {
-    let store = CNContactStore()
-    
-    @Published var showAlert = false
-    @Published var alertMessage = ""
+class ContactsManager {
+    @State private var showAlert = false
+    @State private var alertMessage = ""
     
     func requestContactsAuthorization() {
+        let store = CNContactStore()
         store.requestAccess(for: .contacts) { granted, error in
             
             if let error = error {
@@ -36,70 +29,59 @@ class ContactsManager: ObservableObject {
                 DispatchQueue.main.async {
                     print("acesso concedido")
                 }
+            } else {
+                DispatchQueue.main.async {
+                    self.alertMessage = "Permissão para acessar contatos foi negada. Vá até as Preferências do Sistema para permitir o acesso."
+                    self.showAlert = true
+                }
             }
         }
     }
     
-    func updateClientContact(client: Client, oldClientEmail: String) throws {
-        let predicate = CNContact.predicateForContacts(matchingEmailAddress: oldClientEmail)
-        let keysToFetch = [CNContactGivenNameKey, CNContactImageDataKey, CNContactPhoneNumbersKey, CNContactEmailAddressesKey, CNContactJobTitleKey] as [CNKeyDescriptor]
-        
-        let contacts = try store.unifiedContacts(matching: predicate, keysToFetch: keysToFetch)
-        
-        guard let contact = contacts.first else {
-            throw NSError(domain: "Contact not found", code: 404, userInfo: nil)
-        }
-                
-        let mutableContact = contact.mutableCopy() as! CNMutableContact
-        
-        mutableContact.givenName = client.name
-        mutableContact.phoneNumbers = [CNLabeledValue(label: CNLabelPhoneNumberMain, value: CNPhoneNumber(stringValue: client.cellphone))]
-        mutableContact.emailAddresses = [CNLabeledValue(label: CNLabelHome, value: client.email as NSString)]
-        mutableContact.imageData = client.photo
-        mutableContact.jobTitle = client.occupation
-        
-        let saveRequest = CNSaveRequest()
-        saveRequest.update(mutableContact)
-        try store.execute(saveRequest)
-    }
-    
-    func createContact(name: String, cellphone: String, email: String, photo: Data, occupation: String) -> CNMutableContact {
+    func createContact(name: String, cellphone: String, email: String/*, photo: Data*/, occupation: String) -> CNMutableContact {
         let contact = CNMutableContact()
         
         // Configura os dados do cliente
         contact.givenName = name
         contact.phoneNumbers = [CNLabeledValue(label: CNLabelPhoneNumberMobile, value: CNPhoneNumber(stringValue: cellphone))]
         contact.emailAddresses = [CNLabeledValue(label: CNLabelWork, value: email as NSString)]
-        contact.imageData = photo
+        //contact.imageData = photo
         contact.jobTitle = occupation
         return contact
     }
+    
+//    func createCNMutableContact(client: Client) -> CNMutableContact {
+//
+//    }
     
     //Quando clicar no botao de adicionar contato
     func checkContactsAuthorizationAndSave(contact: CNMutableContact) {
         let authorizationStatus = CNContactStore.authorizationStatus(for: .contacts)
         
         switch authorizationStatus {
-            
         case .notDetermined:
+            //Se a permissão não foi determinada, solicita o acesso
             requestContactsAuthorization()
             print("Pediu o acesso novamente")
         case .authorized:
+            //Se já foi autorizado, adiciona o contato
             print("Autorizado")
             saveContact(contact: contact)
         case .denied:
-            alertMessage = AlertMessageEnum.accessDenied.rawValue
+            //Se o acesso foi negado, sugere ir para os ajustes
+            alertMessage = "Acesso aos contatos foi negado. Vá até as Preferências do Sistema > Segurança e Privacidade para conceder acesso ao app."
             showAlert = true
         case .restricted:
-            alertMessage = AlertMessageEnum.accessRestricted.rawValue
+            alertMessage = "O acesso aos contatos está restrito e não pode ser autorizado."
             showAlert = true
         @unknown default:
-            alertMessage = AlertMessageEnum.unknownError.rawValue
+            alertMessage = "Erro desconhecido ao acessar contatos."
             showAlert = true
         }
     }
     
     func saveContact(contact: CNMutableContact) {
+        let store = CNContactStore()
         let saveRequest = CNSaveRequest()
         saveRequest.add(contact, toContainerWithIdentifier: nil)
         
@@ -108,8 +90,28 @@ class ContactsManager: ObservableObject {
             print("salvo com sucesso")
         } catch {
             print("Erro ao salvar o contato: \(error.localizedDescription)")
+            // Handle error
         }
     }
+    
+    func updateContact(contact: CNMutableContact) {
+        guard let mutableContact = contact.mutableCopy() as? CNMutableContact else { return }
+    }
+    
+//    func fetchContactBy(name: String) {
+//        // limit the contact properties to fetch
+//        let keysToFetch = [CNContactGivenNameKey, CNContactFamilyNameKey] as [CNKeyDescriptor]
+//
+//        let store = CNContactStore()
+//        do {
+//            let predicate = CNContact.predicateForContacts(matchingName: name)
+//            let contacts = try store.unifiedContacts(matching: predicate, keysToFetch: keysToFetch)
+//            print("Fetched contacts: \(contacts)")
+//        } catch {
+//            print("Failed to fetch contact, error: \(error)")
+//            // Handle error
+//        }
+//    }
     
 }
 
