092481dc28b2acf1b6045fb6195b7dbdb7169a63
Giovanna Micher Santana <111765178+citricgio@users.noreply.github.com>
2024-10-24 14:43:52 -0300

    Detalhes comentados e pronto para main

--- LawSuit/LawSuit/Views/DetailedLawSuitView.swift
+++ LawSuit/LawSuit/Views/DetailedLawSuitView.swift
@@ -43,8 +43,6 @@ struct DetailedLawSuitView: View {
                     HStack(alignment: .top, spacing: 22) {
                         mainBlock
                         VStack(spacing: 10) {
-                            CustomSegmentedControl(selectedOption: $selectedSegment, infos: ["Movimentações", "Notas"])
-
                             MovimentationBlock(dataViewModel: _dataViewModel, lawsuit: lawsuit)
                                 .frame(maxHeight: .infinity)
                         }
@@ -227,6 +225,8 @@ extension DetailedLawSuitView {
 										.bold()
 										.underline(dataViewModel.coreDataManager.entityManager.authorIsEntity(lawsuit: lawsuit))
 							  }
+                              .buttonStyle(.plain)
+                              .underline()
                         
                     }
                     Spacer()
